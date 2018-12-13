c4299f7e93b7040386f80463e0885ff907bfe613
David Segura <david@segura.org>
2018-12-13 01:08:56 -0600

    18.12.13.0

---
+++ Content/Scripts/Remove-OneDriveSetup.ps1
@@ -0,0 +1,71 @@
+#   David Segura
+#   http://osdeploy.com
+#
+#   OSBuilder Script
+#   Remove-OneDriveSetup.ps1
+
+#======================================================================================
+#   Remove Files
+#======================================================================================
+if (Test-Path "$MountDirectory\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk") {
+    Remove-Item -Path "$MountDirectory\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" -Force | Out-Null
+}
+
+#======================================================================================
+#   Load Registry Hives
+#======================================================================================
+$RegDefault = "$MountDirectory\Windows\System32\Config\Default"
+if (Test-Path $RegDefault) {
+    Write-Host "Loading $RegDefault" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountDefault $RegDefault" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegDefaultUser = "$MountDirectory\Users\Default\ntuser.dat"
+if (Test-Path $RegDefaultUser) {
+    Write-Host "Loading $RegDefaultUser" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountDefaultUser $RegDefaultUser" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegSoftware = "$MountDirectory\Windows\System32\Config\Software"
+if (Test-Path $RegSoftware) {
+    Write-Host "Loading $RegSoftware" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountSoftware $RegSoftware" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+$RegSystem = "$MountDirectory\Windows\System32\Config\System"
+if (Test-Path $RegSystem) {
+    Write-Host "Loading $RegSystem" -ForegroundColor Cyan
+    Start-Process reg -ArgumentList "load HKLM\MountSystem $RegSystem" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
+
+#======================================================================================
+#   Registry Commands
+#======================================================================================
+$RegCommands =
+'delete HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v OneDriveSetup /f'
+
+foreach ($Command in $RegCommands) {
+    if ($Command -like "*HKCU*") {
+        $Command = $Command -replace "HKCU","HKLM\MountDefaultUser"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    } elseif ($Command -like "*HKLM\Software*") {
+        $Command = $Command -replace "HKLM\Software","HKLM\MountSoftware"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    } elseif ($Command -like "*HKLM\System*") {
+        $Command = $Command -replace "HKLM\System","HKLM\MountSystem"
+        Write-Host "reg $Command" -ForegroundColor Green
+        Start-Process reg -ArgumentList $Command -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+    }
+}
+
+#======================================================================================
+#   Unload Registry Hives
+#======================================================================================
+Start-Process reg -ArgumentList "unload HKLM\MountDefault" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountDefaultUser" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountSoftware" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+Start-Process reg -ArgumentList "unload HKLM\MountSystem" -Wait -WindowStyle Hidden -ErrorAction SilentlyContinue
+
+#======================================================================================
+#   Testing
+#======================================================================================
+#   [void](Read-Host 'Press Enter to continue')
